cf031fadd9805f2e634b0d201bdf6b2291c71137
Chris Smith <chris@cj-dev.co.uk>
2012-05-24 09:48:02 +0100

    Stylesheet Issue

--- _vti_cnf/index.php
+++ _vti_cnf/index.php
@@ -1,19 +1,19 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|24 May 2012 08:43:51 -0000
+vti_timelastmodified:TR|24 May 2012 08:47:26 -0000
 vti_title:SR|LTU Support Centre
 vti_author:SR|STUDENT\\c.smith
 vti_modifiedby:SR|STUDENT\\c.smith
-vti_nexttolasttimemodified:TW|30 Apr 2012 14:37:36 -0000
+vti_nexttolasttimemodified:TW|24 May 2012 08:43:51 -0000
 vti_timecreated:TR|06 Jan 2012 11:20:49 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_syncwith_localhost\\p\:\\dev\\metro/p\:/dev/metro:TR|05 Jan 2012 15:39:01 -0000
 vti_backlinkinfo:VX|index.php logout.php
-vti_cacheddtm:TX|24 May 2012 08:43:51 -0000
-vti_filesize:IR|5827
+vti_cacheddtm:TX|24 May 2012 08:47:26 -0000
+vti_filesize:IR|6199
 vti_cachedtitle:SR|LTU Support Centre
 vti_cachedbodystyle:SR|<body>
-vti_cachedlinkinfo:VX|Q|main.css Q|metro.css Q|about.css Q|pie.css Q|admin.css Q|admin_table.css Q|message_boxes.css Q|icons.css H|http://www.barnsley-ltu.co.uk/img/favicon.ico S|admin.js S|adminMoodleControl.js I|scripts/import_fancybox.php I|views/page_header.php I|views/metro_navigation.php I|views/admin_support_requests.php I|views/admin_reports_list.php I|views/admin_all_support_requests.php I|views/admin_closed_reports.php I|views/admin_training_list.php I|views/admin_training_requests.php I|views/message_box.php I|views/admin_moodle_list.php I|views/admin_admin_users.php K|index.php
-vti_cachedsvcrellinks:VX|NQUS|main.css NQUS|metro.css NQUS|about.css NQUS|pie.css NQUS|admin.css NQUS|admin_table.css NQUS|message_boxes.css NQUS|icons.css NHHS|http://www.barnsley-ltu.co.uk/img/favicon.ico NSUS|admin.js NSUS|adminMoodleControl.js FIUS|scripts/import_fancybox.php FIUS|views/page_header.php FIUS|views/metro_navigation.php FIUS|views/admin_support_requests.php FIUS|views/admin_reports_list.php FIUS|views/admin_all_support_requests.php FIUS|views/admin_closed_reports.php FIUS|views/admin_training_list.php FIUS|views/admin_training_requests.php FIUS|views/message_box.php FIUS|views/admin_moodle_list.php FIUS|views/admin_admin_users.php FKUS|index.php
+vti_cachedlinkinfo:VX|Q|http://www.barnsley-ltu.co.uk/styles/main.css Q|http://www.barnsley-ltu.co.uk/styles/metro.css Q|http://www.barnsley-ltu.co.uk/styles/about.css Q|http://www.barnsley-ltu.co.uk/styles/pie.css Q|http://www.barnsley-ltu.co.uk/styles/admin.css Q|http://www.barnsley-ltu.co.uk/styles/admin_table.css Q|http://www.barnsley-ltu.co.uk/styles/message_boxes.css Q|http://www.barnsley-ltu.co.uk/styles/icons.css H|http://www.barnsley-ltu.co.uk/img/favicon.ico S|http://www.barnsley-ltu.co.uk/scripts/admin.js S|http://www.barnsley-ltu.co.uk/scripts/adminMoodleControl.js I|scripts/import_fancybox.php I|views/page_header.php I|views/metro_navigation.php I|views/admin_support_requests.php I|views/admin_reports_list.php I|views/admin_all_support_requests.php I|views/admin_closed_reports.php I|views/admin_training_list.php I|views/admin_training_requests.php I|views/message_box.php I|views/admin_moodle_list.php I|views/admin_admin_users.php K|index.php
+vti_cachedsvcrellinks:VX|NQHS|http://www.barnsley-ltu.co.uk/styles/main.css NQHS|http://www.barnsley-ltu.co.uk/styles/metro.css NQHS|http://www.barnsley-ltu.co.uk/styles/about.css NQHS|http://www.barnsley-ltu.co.uk/styles/pie.css NQHS|http://www.barnsley-ltu.co.uk/styles/admin.css NQHS|http://www.barnsley-ltu.co.uk/styles/admin_table.css NQHS|http://www.barnsley-ltu.co.uk/styles/message_boxes.css NQHS|http://www.barnsley-ltu.co.uk/styles/icons.css NHHS|http://www.barnsley-ltu.co.uk/img/favicon.ico NSHS|http://www.barnsley-ltu.co.uk/scripts/admin.js NSHS|http://www.barnsley-ltu.co.uk/scripts/adminMoodleControl.js FIUS|scripts/import_fancybox.php FIUS|views/page_header.php FIUS|views/metro_navigation.php FIUS|views/admin_support_requests.php FIUS|views/admin_reports_list.php FIUS|views/admin_all_support_requests.php FIUS|views/admin_closed_reports.php FIUS|views/admin_training_list.php FIUS|views/admin_training_requests.php FIUS|views/message_box.php FIUS|views/admin_moodle_list.php FIUS|views/admin_admin_users.php FKUS|index.php
 vti_cachedneedsrewrite:BR|true
 vti_cachedhasbots:BR|true
 vti_cachedhastheme:BR|false
