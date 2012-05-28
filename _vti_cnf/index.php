84980b63c7eb9be3aee28d4b47954dc704b2db16
Chris Smith <chris@cj-dev.co.uk>
2012-05-28 09:16:25 +0100

    ssh added to some files

--- _vti_cnf/index.php
+++ _vti_cnf/index.php
@@ -1,19 +1,19 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|24 May 2012 08:47:26 -0000
+vti_timelastmodified:TR|25 May 2012 11:03:20 -0000
 vti_title:SR|LTU Support Centre
 vti_author:SR|STUDENT\\c.smith
 vti_modifiedby:SR|STUDENT\\c.smith
-vti_nexttolasttimemodified:TW|24 May 2012 08:43:51 -0000
+vti_nexttolasttimemodified:TW|24 May 2012 08:47:26 -0000
 vti_timecreated:TR|06 Jan 2012 11:20:49 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_syncwith_localhost\\p\:\\dev\\metro/p\:/dev/metro:TR|05 Jan 2012 15:39:01 -0000
 vti_backlinkinfo:VX|index.php logout.php
-vti_cacheddtm:TX|24 May 2012 08:47:26 -0000
-vti_filesize:IR|6199
+vti_cacheddtm:TX|25 May 2012 11:03:20 -0000
+vti_filesize:IR|6210
 vti_cachedtitle:SR|LTU Support Centre
 vti_cachedbodystyle:SR|<body>
-vti_cachedlinkinfo:VX|Q|http://www.barnsley-ltu.co.uk/styles/main.css Q|http://www.barnsley-ltu.co.uk/styles/metro.css Q|http://www.barnsley-ltu.co.uk/styles/about.css Q|http://www.barnsley-ltu.co.uk/styles/pie.css Q|http://www.barnsley-ltu.co.uk/styles/admin.css Q|http://www.barnsley-ltu.co.uk/styles/admin_table.css Q|http://www.barnsley-ltu.co.uk/styles/message_boxes.css Q|http://www.barnsley-ltu.co.uk/styles/icons.css H|http://www.barnsley-ltu.co.uk/img/favicon.ico S|http://www.barnsley-ltu.co.uk/scripts/admin.js S|http://www.barnsley-ltu.co.uk/scripts/adminMoodleControl.js I|scripts/import_fancybox.php I|views/page_header.php I|views/metro_navigation.php I|views/admin_support_requests.php I|views/admin_reports_list.php I|views/admin_all_support_requests.php I|views/admin_closed_reports.php I|views/admin_training_list.php I|views/admin_training_requests.php I|views/message_box.php I|views/admin_moodle_list.php I|views/admin_admin_users.php K|index.php
-vti_cachedsvcrellinks:VX|NQHS|http://www.barnsley-ltu.co.uk/styles/main.css NQHS|http://www.barnsley-ltu.co.uk/styles/metro.css NQHS|http://www.barnsley-ltu.co.uk/styles/about.css NQHS|http://www.barnsley-ltu.co.uk/styles/pie.css NQHS|http://www.barnsley-ltu.co.uk/styles/admin.css NQHS|http://www.barnsley-ltu.co.uk/styles/admin_table.css NQHS|http://www.barnsley-ltu.co.uk/styles/message_boxes.css NQHS|http://www.barnsley-ltu.co.uk/styles/icons.css NHHS|http://www.barnsley-ltu.co.uk/img/favicon.ico NSHS|http://www.barnsley-ltu.co.uk/scripts/admin.js NSHS|http://www.barnsley-ltu.co.uk/scripts/adminMoodleControl.js FIUS|scripts/import_fancybox.php FIUS|views/page_header.php FIUS|views/metro_navigation.php FIUS|views/admin_support_requests.php FIUS|views/admin_reports_list.php FIUS|views/admin_all_support_requests.php FIUS|views/admin_closed_reports.php FIUS|views/admin_training_list.php FIUS|views/admin_training_requests.php FIUS|views/message_box.php FIUS|views/admin_moodle_list.php FIUS|views/admin_admin_users.php FKUS|index.php
+vti_cachedlinkinfo:VX|Q|https://www.barnsley-ltu.co.uk/styles/main.css Q|https://www.barnsley-ltu.co.uk/styles/metro.css Q|https://www.barnsley-ltu.co.uk/styles/about.css Q|https://www.barnsley-ltu.co.uk/styles/pie.css Q|https://www.barnsley-ltu.co.uk/styles/admin.css Q|https://www.barnsley-ltu.co.uk/styles/admin_table.css Q|https://www.barnsley-ltu.co.uk/styles/message_boxes.css Q|https://www.barnsley-ltu.co.uk/styles/icons.css H|https://www.barnsley-ltu.co.uk/img/favicon.ico S|https://www.barnsley-ltu.co.uk/scripts/admin.js S|https://www.barnsley-ltu.co.uk/scripts/adminMoodleControl.js I|scripts/import_fancybox.php I|views/page_header.php I|views/metro_navigation.php I|views/admin_support_requests.php I|views/admin_reports_list.php I|views/admin_all_support_requests.php I|views/admin_closed_reports.php I|views/admin_training_list.php I|views/admin_training_requests.php I|views/message_box.php I|views/admin_moodle_list.php I|views/admin_admin_users.php K|index.php
+vti_cachedsvcrellinks:VX|NQSS|https://www.barnsley-ltu.co.uk/styles/main.css NQSS|https://www.barnsley-ltu.co.uk/styles/metro.css NQSS|https://www.barnsley-ltu.co.uk/styles/about.css NQSS|https://www.barnsley-ltu.co.uk/styles/pie.css NQSS|https://www.barnsley-ltu.co.uk/styles/admin.css NQSS|https://www.barnsley-ltu.co.uk/styles/admin_table.css NQSS|https://www.barnsley-ltu.co.uk/styles/message_boxes.css NQSS|https://www.barnsley-ltu.co.uk/styles/icons.css NHSS|https://www.barnsley-ltu.co.uk/img/favicon.ico NSSS|https://www.barnsley-ltu.co.uk/scripts/admin.js NSSS|https://www.barnsley-ltu.co.uk/scripts/adminMoodleControl.js FIUS|scripts/import_fancybox.php FIUS|views/page_header.php FIUS|views/metro_navigation.php FIUS|views/admin_support_requests.php FIUS|views/admin_reports_list.php FIUS|views/admin_all_support_requests.php FIUS|views/admin_closed_reports.php FIUS|views/admin_training_list.php FIUS|views/admin_training_requests.php FIUS|views/message_box.php FIUS|views/admin_moodle_list.php FIUS|views/admin_admin_users.php FKUS|index.php
 vti_cachedneedsrewrite:BR|true
 vti_cachedhasbots:BR|true
 vti_cachedhastheme:BR|false
--- scripts/_vti_cnf/import_fancybox.php
+++ scripts/_vti_cnf/import_fancybox.php
@@ -1,15 +1,15 @@
 vti_encoding:SR|utf8-nl
 vti_author:SR|STUDENT\\c.smith
 vti_modifiedby:SR|STUDENT\\c.smith
-vti_timelastmodified:TR|23 Mar 2012 09:18:33 -0000
+vti_timelastmodified:TR|25 May 2012 11:05:42 -0000
 vti_timecreated:TR|09 Mar 2012 10:06:30 -0000
 vti_extenderversion:SR|12.0.0.0
 vti_backlinkinfo:VX|event_admin.php index.php invite_users.php planning_page.php user_profile_new.php event.php
-vti_nexttolasttimemodified:TR|20 Mar 2012 14:25:22 -0000
-vti_cacheddtm:TX|23 Mar 2012 09:18:33 -0000
-vti_filesize:IR|716
-vti_cachedlinkinfo:VX|S|http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js S|http://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.mousewheel-3.0.4.pack.js S|http://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.pack.js Q|http://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.css
-vti_cachedsvcrellinks:VX|NSHS|http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js NSHS|http://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.mousewheel-3.0.4.pack.js NSHS|http://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.pack.js NQHS|http://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.css
+vti_nexttolasttimemodified:TW|25 May 2012 11:05:27 -0000
+vti_cacheddtm:TX|25 May 2012 11:05:42 -0000
+vti_filesize:IR|720
+vti_cachedlinkinfo:VX|S|http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js S|https://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.mousewheel-3.0.4.pack.js S|https://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.pack.js Q|https://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.css
+vti_cachedsvcrellinks:VX|NSHS|http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js NSSS|https://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.mousewheel-3.0.4.pack.js NSSS|https://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.pack.js NQSS|https://www.barnsley-ltu.co.uk/scripts/fancybox/fancybox/jquery.fancybox-1.3.4.css
 vti_cachedneedsrewrite:BR|false
 vti_cachedhasbots:BR|false
 vti_cachedhastheme:BR|false
